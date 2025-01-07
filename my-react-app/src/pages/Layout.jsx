43d01e4979f0aef0d108bc53492af10a400d7d1e
Ssswdrs <saksornsawatdee@gmail.com>
2025-01-08 01:17:59 +0700

    rename and change port 5173 to 8000

--- my-react-app/src/pages/Layout.jsx
+++ my-react-app/src/pages/Layout.jsx
@@ -12,7 +12,7 @@ function Layout() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>layouts</h1>
+      <h1>login</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
